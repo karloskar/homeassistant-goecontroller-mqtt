dcf1e445216b0136df9f57cda184e6d81e441126
KO. Mattsson <karloskarmattsson@gmail.com>
2023-12-23 05:07:19 +0100

    Formatting

--- custom_components/goecontroller_mqtt/config_flow.py
+++ custom_components/goecontroller_mqtt/config_flow.py
@@ -116,14 +116,10 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             description_placeholders={"name": name},
         )
 
-    async def async_step_user(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
+    async def async_step_user(self, user_input: dict[str, Any] | None = None) -> FlowResult:
         """Handle the initial step."""
         if user_input is None:
-            return self.async_show_form(
-                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
-            )
+            return self.async_show_form(step_id="user", data_schema=STEP_USER_DATA_SCHEMA)
 
         errors = {}
 
